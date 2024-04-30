a5acdb1e4c69dddce2693562611a173563014bed
Seth <58447309+ohmygaugh-crypto@users.noreply.github.com>
2024-04-30 09:20:49 -0400

    fixed button funciton

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Define handleClick function
+  const handleClick = () => {
+    console.log('Button clicked!');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
